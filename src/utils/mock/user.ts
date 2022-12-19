4cda9573cfea56d681cfe783e199f21a5a16a620
BillRizer <teliz.pablo@gmail.com>
2022-12-19 01:22:50 -0300

    temp(user): add mock for user entity

---
+++ src/utils/mock/user.ts
@@ -0,0 +1,12 @@
+import { User } from '../../user/entities/user.entity';
+
+export const mockUser: User = {
+  email: 'test@example.com',
+  id: 'asdaa-asda-sdas-das',
+  credit: 0,
+  name: 'Fake name',
+  password: 'hashedPassword',
+  createdAt: '',
+  updatedAt: '',
+  deletedAt: '',
+};
