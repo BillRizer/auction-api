65edf5cdd19e981ad7cca46c96ff77109b2875e5
BillRizer <teliz.pablo@gmail.com>
2022-12-19 01:30:13 -0300

    feat(user): implement service for create new user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { EncryptService } from '../encrypt/encrypt.service';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -18,5 +19,25 @@ export class UserService {
       return user;
     }
     return null;
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User | null> {
+    try {
+      if (await this.findByEmail(createUserDto.email)) {
+        throw new Error('Email address already exists.');
+      }
+      const hashedPassword = await this.encrypt.encrypHash(
+        createUserDto.password,
+      );
+
+      const created = await this.userRepository.create({
+        ...createUserDto,
+        password: hashedPassword,
+      });
+      return await this.userRepository.save(created);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
