8d1b944a5bc10dba417e5d024a668e4d18da9bc5
BillRizer <teliz.pablo@gmail.com>
2022-12-19 00:06:34 -0300

    refactor(user): register user DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty({ message: 'E-mail Field missing' })
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  password?: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Name Field missing' })
+  name: string;
+}
