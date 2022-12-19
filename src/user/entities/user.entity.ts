86f5fcd72f026e6c0c8a07d860b4533973d04a02
BillRizer <teliz.pablo@gmail.com>
2022-12-19 00:00:29 -0300

    refactor(user): add entity for user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { IsNumber, MaxLength, MinLength } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column({ unique: true })
+  public email: string;
+
+  @Column()
+  @Exclude()
+  public password: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  @IsNumber()
+  public credit: number;
+
+  @CreateDateColumn()
+  public createdAt: string;
+
+  @UpdateDateColumn()
+  public updatedAt: string;
+}
