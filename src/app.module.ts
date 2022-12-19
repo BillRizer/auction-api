1ae4e6647ec49a2a8fd71adcf490f91b734d03cc
BillRizer <teliz.pablo@gmail.com>
2022-12-19 01:09:38 -0300

    chore(encrypt): add salt param

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -18,10 +19,12 @@ import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
         POSTGRES_DB: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION_TIME: Joi.string().required(),
+        BCRYPT_SALT: Joi.string().required(),
       }),
     }),
     DatabaseModule,
     AuthModule,
+    UserModule,
   ],
   providers: [
     {
