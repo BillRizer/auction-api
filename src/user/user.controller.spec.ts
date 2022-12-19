7e1946b2a95c3730e65e7a909ea69d5e68186404
BillRizer <teliz.pablo@gmail.com>
2022-12-19 01:04:14 -0300

    test(user): add provider and mock for controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,22 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { mockUser } from '../utils/mock/user';
+
+const newUserData = {
+  ...mockUser,
+};
+const updatedUserData = {
+  ...mockUser,
+  name: 'changed name',
+};
+const newUserEntity = new User(newUserData);
+
+const updatedUserEntity = new User(updatedUserData);
 
 describe('UserController', () => {
   let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([User])],
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(newUserEntity),
+            findOne: jest.fn().mockResolvedValue(newUserEntity),
+            findByEmail: jest.fn(),
+            update: jest.fn().mockResolvedValue(updatedUserEntity),
+          },
+        },
+      ],
     }).compile();
 
     userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
