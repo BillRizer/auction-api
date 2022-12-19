662cac8924c0075997ce6de9062e433a4f812b26
BillRizer <teliz.pablo@gmail.com>
2022-12-19 02:09:46 -0300

    feat(user): implement update in controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,9 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
+  Patch,
   Post,
   Request,
   UnauthorizedException,
@@ -10,6 +13,7 @@ import { ApiTags } from '@nestjs/swagger';
 import RequestWithUser from 'src/auth/interface/request-with-user.interface';
 import { Public } from '../auth/decorator/public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -31,5 +35,21 @@ export class UserController {
       throw new UnauthorizedException();
     }
     return await this.userService.findOne(userId);
+  }
+
+  @Patch()
+  async update(
+    @Request() req: RequestWithUser,
+    @Body() updateCompanyDto: UpdateUserDto,
+  ) {
+    try {
+      const userId = req.user.userId;
+      if (!userId) {
+        throw new UnauthorizedException();
+      }
+      return await this.userService.update(userId, updateCompanyDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
