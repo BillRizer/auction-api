066360312c0ce2583577f605444de29e2673a662
BillRizer <teliz.pablo@gmail.com>
2022-12-19 02:02:09 -0300

    feat(user): implement create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/decorator/public.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
+
+  @Public()
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
 }
