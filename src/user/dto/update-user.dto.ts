928e5cdcc337d311c0be5e08a3da05e97be087ab
BillRizer <teliz.pablo@gmail.com>
2022-12-19 01:41:24 -0300

    refactor(user): add DTO for update user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsOptional()
+  old_password?: string;
+}
