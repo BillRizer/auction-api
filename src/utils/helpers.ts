5af31cca77377b41a4944a68ede1e384c5eb22b2
BillRizer <teliz.pablo@gmail.com>
2022-12-21 00:34:08 -0300

    refactor(utils): add TEST tag support

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 export enum EEnvironments {
   DEV = 'DEV',
+  TEST = 'TEST',
   PROD = 'PROD',
 }
 export const getEnvironment = (): EEnvironments => {
@@ -8,7 +9,9 @@ export const getEnvironment = (): EEnvironments => {
   }
   const env = EEnvironments[process.env.ENV.toUpperCase()];
   if (!env) {
-    throw new Error('Environment need be DEV or PROD (no case sensitivity)');
+    throw new Error(
+      'Environment need be DEV, TEST or PROD (no case sensitivity)',
+    );
   }
   return env;
 };
