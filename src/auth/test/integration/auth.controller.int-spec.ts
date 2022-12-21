2b871f7ef1b56c408878ff80c443af61edc3bf03
BillRizer <teliz.pablo@gmail.com>
2022-12-21 20:37:55 -0300

    test(auth): [GET]/auth/login Should receive jwt token

---
+++ src/auth/test/integration/auth.controller.int-spec.ts
@@ -0,0 +1,85 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { ConfigModule } from '@nestjs/config';
+import { AppModule } from '../../../app.module';
+import { CreateUserDto } from '../../../user/dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from '../../../user/entities/user.entity';
+import { DatabaseModule } from '../../../database/database.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserService } from '../../../user/user.service';
+import { UpdateUserDto } from '../../../user/dto/update-user.dto';
+import { envFilePath } from '../../../utils/helpers';
+import { join } from 'path';
+import { userStub } from '../../../user/test/stubs/user.stub';
+
+describe('AuthController (integration)', () => {
+  let app: INestApplication;
+  let httpServer: any;
+  let userRepository: Repository<User>;
+  let userService: UserService;
+
+  const createdUserStub: CreateUserDto = {
+    email: userStub.email,
+    name: userStub.name,
+    credit: userStub.credit,
+    password: userStub.password,
+  };
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        DatabaseModule,
+        ConfigModule.forRoot({
+          envFilePath: [
+            join(envFilePath, '.env'),
+            join(envFilePath, '.env.test'),
+          ],
+        }),
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+    httpServer = app.getHttpServer();
+    userRepository = app.get<Repository<User>>(getRepositoryToken(User));
+    userService = app.get<UserService>(UserService);
+  });
+
+  afterAll(async () => {
+    await userRepository.clear();
+    app.close();
+  });
+
+  describe('/auth/login [POST] (integration)', () => {
+    it('it should receive jwt token', async () => {
+      await userService.create(createdUserStub);
+
+      const jwt = await getJwtToken(
+        httpServer,
+        createdUserStub.email,
+        createdUserStub.password,
+      );
+      expect(jwt.length).toBeGreaterThan(30);
+    });
+  });
+});
+
+export async function getJwtToken(httpServer, email: string, password: string) {
+  let jwtToken = '';
+  try {
+    await request(httpServer)
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send({ email, password })
+      .expect(HttpStatus.OK)
+      .then((response) => {
+        jwtToken = response.body.jwt;
+      });
+    return jwtToken;
+  } catch (error) {
+    return null;
+  }
+}
