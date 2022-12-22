dc2fc12725cfe591a25aa625e9f83d4679bcfdbc
BillRizer <teliz.pablo@gmail.com>
2022-12-22 15:00:02 -0300

    refactor(product): add DTO for response after create product

---
+++ src/product/dto/response-created-product.dto.ts
@@ -0,0 +1,12 @@
+export class ResponseCreatedProduct {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  user_id: string;
+  availableForAuction: boolean;
+  sold: boolean;
+  endsAt: string;
+  createdAt: string;
+  updatedAt: string;
+}
