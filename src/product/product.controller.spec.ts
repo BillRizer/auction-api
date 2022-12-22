4e66dbfc898eddb273839b31b8158fd3f8290f5b
BillRizer <teliz.pablo@gmail.com>
2022-12-22 14:58:57 -0300

    test(product): productController Should create new product

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,9 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { userStub } from '../user/test/stubs/user.stub';
+import RequestWithUser from '../auth/interface/request-with-user.interface';
+import { CreateProductDto } from './dto/create-product.dto';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import {
+  productEntityStub,
+  responseCreatedProduct,
+} from './test/stubs/product.stub';
+import { ResponseCreatedProduct } from './dto/response-created-product.dto';
 
 describe('ProductController', () => {
-  let controller: ProductController;
+  let productController: ProductController;
+  let productService: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,17 +20,36 @@ describe('ProductController', () => {
       providers: [
         {
           provide: ProductService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockResolvedValue(productEntityStub),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<ProductController>(ProductController);
+    productController = module.get<ProductController>(ProductController);
+    productService = module.get<ProductService>(ProductService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(productController).toBeDefined();
+    expect(productService).toBeDefined();
   });
 
-  it('Should create new product', async () => {});
+  describe('productController', () => {
+    it('Should create new product', async () => {
+      const req = { user: { userId: 'id-mocked' } } as Partial<RequestWithUser>;
+      const newProduct: CreateProductDto = {
+        ...productEntityStub,
+        user: { id: productEntityStub.id },
+      };
+
+      const created = await productController.create(
+        req as RequestWithUser,
+        newProduct,
+      );
+
+      expect(created).toEqual(responseCreatedProduct);
+    });
+  });
 });
