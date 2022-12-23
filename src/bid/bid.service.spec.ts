4d80aa398ca11c096560b529c82706b62eb049d2
BillRizer <teliz.pablo@gmail.com>
2022-12-23 13:13:40 -0300

    test(bid): bidService should create new bid and return object

--- src/bid/bid.service.spec.ts
+++ src/bid/bid.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { BidService } from './bid.service';
+import { CreateBidDto } from './dto/create-bid.dto';
+import { Bid } from './entities/bid.entity';
+import { bidEntityStub, createBidStub } from './test/stubs/bid.stub';
 
 describe('BidService', () => {
-  let service: BidService;
+  let bidService: BidService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BidService],
+      providers: [
+        BidService,
+        {
+          provide: getRepositoryToken(Bid),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn().mockResolvedValue(bidEntityStub),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<BidService>(BidService);
+    bidService = module.get<BidService>(BidService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(bidService).toBeDefined();
+  });
+  describe('bidService', () => {
+    it('should create new bid and return', async () => {
+      const created = await bidService.create(
+        bidEntityStub.userId,
+        bidEntityStub.productId,
+        createBidStub,
+      );
+
+      expect(created).toEqual(bidEntityStub);
+    });
+   
   });
 });
