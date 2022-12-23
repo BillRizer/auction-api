5c6d3f76eb53042a113ab406ff4f5fc325d3a7ac
BillRizer <teliz.pablo@gmail.com>
2022-12-23 12:42:42 -0300

    feat(bid): implement bid entity

--- src/bid/entities/bid.entity.ts
+++ src/bid/entities/bid.entity.ts
@@ -1,1 +1,48 @@
-export class Bid {}
+import { Exclude } from 'class-transformer';
+import { IsNumber, IsUUID } from 'class-validator';
+import { Product } from 'src/product/entities/product.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Bid {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column({ nullable: false })
+  public value: number;
+
+  @IsUUID()
+  @Column({ nullable: false, name: 'user_id' })
+  public userId: string;
+
+  @IsUUID()
+  @Column({ nullable: false, name: 'product_id' })
+  public productId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  public updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  public deletedAt: string;
+
+  constructor(bid?: Partial<Bid>) {
+    this.id = bid?.id;
+    this.value = bid?.value;
+    this.createdAt = bid?.createdAt;
+    this.updatedAt = bid?.updatedAt;
+    this.deletedAt = bid?.deletedAt;
+  }
+}
