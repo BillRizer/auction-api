755c192a5de4af9d2b5669672480176a6028864f
BillRizer <teliz.pablo@gmail.com>
2022-12-23 13:17:28 -0300

    feat(bid): bidService implement create

--- src/bid/bid.service.ts
+++ src/bid/bid.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateBidDto } from './dto/create-bid.dto';
+import { Bid } from './entities/bid.entity';
+import { BidNotCreatedException } from './exceptions/bid-not-created.execption';
 
 @Injectable()
 export class BidService {
-  create(createBidDto: CreateBidDto) {
-    return 'This action adds a new bid';
+  constructor(
+    @InjectRepository(Bid)
+    private bidRepository: Repository<Bid>,
+  ) {}
+  async create(userId: string, productId: string, createBidDto: CreateBidDto) {
+    try {
+      const created = await this.bidRepository.create({
+        ...createBidDto,
+        userId,
+        productId,
+      });
+      return await this.bidRepository.save(created);
+    } catch (error) {
+      //TODO log here
+      throw new BidNotCreatedException();
+    }
   }
 
   findAll() {
