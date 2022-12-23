fb6e5e1c82f9ca791d4ecb6e0312b5d7f43a1dd6
BillRizer <teliz.pablo@gmail.com>
2022-12-23 01:06:57 -0300

    test(product): [POST]/product should create a product and return object

--- src/product/test/integration/product.controller.int-spec.ts
+++ src/product/test/integration/product.controller.int-spec.ts
@@ -11,12 +11,27 @@ import { join } from 'path';
 import { getJwtToken } from '../../../auth/test/integration/auth.controller.int-spec';
 import { Product } from '../../../product/entities/product.entity';
 import { ProductService } from '../../../product/product.service';
+import { UserService } from '../../../user/user.service';
+import { CreateUserDto } from '../../../user/dto/create-user.dto';
+import { userStub } from '../../../user/test/stubs/user.stub';
+import {
+  createProductStub,
+  responseCreatedProduct,
+} from '../stubs/product.stub';
 
-describe('UserController (integration)', () => {
+describe('ProductController (integration)', () => {
   let app: INestApplication;
   let httpServer: any;
   let productRepository: Repository<Product>;
   let productService: ProductService;
+  let userService: UserService;
+
+  const createdUserStub: CreateUserDto = {
+    email: userStub.email,
+    name: userStub.name,
+    credit: userStub.credit,
+    password: userStub.password,
+  };
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -39,9 +54,53 @@ describe('UserController (integration)', () => {
       getRepositoryToken(Product),
     );
     productService = app.get<ProductService>(ProductService);
+    userService = app.get<UserService>(UserService);
   });
   it('should be defined', () => {
     expect(productRepository).toBeDefined();
     expect(productService).toBeDefined();
   });
+
+  describe('/product [POST] (integration)', () => {
+    let jwtToken = '';
+    beforeAll(async () => {
+      await userService.create(createdUserStub);
+      //TODO refactor this, duplicate code for get jwt token
+      jwtToken = await getJwtToken(
+        httpServer,
+        createdUserStub.email,
+        createdUserStub.password,
+      );
+    });
+    beforeEach(async () => {
+      await cleanProductTable(productRepository);
+    });
+
+    it('it should create a product and return object', async () => {
+      await request(httpServer)
+        .post('/product')
+        .set('Accept', 'application/json')
+        .set({
+          Authorization: `Bearer ${jwtToken}`,
+        })
+        .send(createProductStub)
+        .expect((response: request.Response) => {
+          const { availableForAuction, category, description, name, sold } =
+            response.body;
+
+          expect(availableForAuction).toEqual(
+            responseCreatedProduct.availableForAuction,
+          );
+          expect(category).toEqual(responseCreatedProduct.category);
+          expect(description).toEqual(responseCreatedProduct.description);
+          expect(name).toEqual(responseCreatedProduct.name);
+          expect(sold).toEqual(responseCreatedProduct.sold);
+        })
+        .expect(HttpStatus.CREATED);
+    });
+  });
 });
+
+export async function cleanProductTable(productRepository) {
+  await productRepository.query(`TRUNCATE "product" CASCADE;`);
+}
