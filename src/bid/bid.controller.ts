28e7823ee92e3cd9c4145f15e7bf1379144e736f
BillRizer <teliz.pablo@gmail.com>
2022-12-24 01:06:26 -0300

    feat(bid): implement create and findAllByProductId

--- src/bid/bid.controller.ts
+++ src/bid/bid.controller.ts
@@ -6,21 +6,34 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  ParseUUIDPipe,
+  HttpCode,
+  HttpException,
 } from '@nestjs/common';
+import RequestWithUser from 'src/auth/interface/request-with-user.interface';
 import { BidService } from './bid.service';
 import { CreateBidDto } from './dto/create-bid.dto';
+import { BidNotCreatedException } from './exceptions/bid-not-created.execption';
 
 @Controller('bid')
 export class BidController {
   constructor(private readonly bidService: BidService) {}
 
-  @Post()
-  create(@Body() createBidDto: CreateBidDto) {
-    return this.bidService.create(createBidDto);
+  @Post(':id')
+  @HttpCode(201)
+  async create(
+    @Request() req: RequestWithUser,
+    @Param('id', ParseUUIDPipe) productId: string,
+    @Body() createBidDto: CreateBidDto,
+  ) {
+    const userId = req?.user?.userId;
+
+    await this.bidService.create(userId, productId, createBidDto);
   }
 
-  @Get()
-  findAll() {
-    return this.bidService.findAll();
+  @Get(':id')
+  async findAllByProductId(@Param('id', ParseUUIDPipe) productId: string) {
+    return await this.bidService.findAllByProductId(productId);
   }
 }
