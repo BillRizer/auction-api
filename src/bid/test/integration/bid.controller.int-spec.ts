fb4dc050d3ad0859fdef51dd6faa70d7539e3d7f
BillRizer <teliz.pablo@gmail.com>
2022-12-28 02:21:43 -0300

    test(bid): [POST]/bid/:id should create a bid for product

--- src/bid/test/integration/bid.controller.int-spec.ts
+++ src/bid/test/integration/bid.controller.int-spec.ts
@@ -4,7 +4,6 @@ import * as request from 'supertest';
 import { ConfigModule } from '@nestjs/config';
 import { AppModule } from '../../../app.module';
 import { Repository } from 'typeorm';
-import { DatabaseModule } from '../../../database/database.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { envFilePath } from '../../../utils/helpers';
 import { join } from 'path';
@@ -16,11 +15,121 @@ import { CreateUserDto } from '../../../user/dto/create-user.dto';
 import { userStub } from '../../../user/test/stubs/user.stub';
 
 import { getUserInfo } from '../../../user/test/integration/user.controller.int-spec';
-import { BidService } from '../../bid.service';
-import { Bid } from '../../entities/bid.entity';
+import { Bid } from '../../../bid/entities/bid.entity';
+import { createProductStub } from '../../../product/test/stubs/product.stub';
+import { CreateBidDto } from '../../../bid/dto/create-bid.dto';
 
 describe('BidController (integration)', () => {
-   it.todo('should ');
+  let app: INestApplication;
+  let httpServer: any;
+  let userService: UserService;
+  let productService: ProductService;
+  let bidRepository: Repository<Bid>;
+  let jwtTokenA = '';
+  let jwtTokenB = '';
+  let userInfoA: any;
+  let userInfoB: any;
+  let productFromUserA: Product;
+
+  const createdUserStubA: CreateUserDto = {
+    email: userStub.email,
+    name: userStub.name,
+    credit: userStub.credit,
+    password: userStub.password,
+  };
+  const createdUserStubB: CreateUserDto = {
+    email: 'b_' + userStub.email,
+    name: 'b_' + userStub.name,
+    credit: userStub.credit,
+    password: userStub.password,
+  };
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+
+        ConfigModule.forRoot({
+          envFilePath: [
+            join(envFilePath, '.env'),
+            join(envFilePath, '.env.test'),
+          ],
+        }),
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+    httpServer = app.getHttpServer();
+    bidRepository = app.get<Repository<Bid>>(getRepositoryToken(Bid));
+    userService = app.get<UserService>(UserService);
+    productService = app.get<ProductService>(ProductService);
+
+    //create user
+    await userService.create(createdUserStubA);
+    await userService.create(createdUserStubB);
+
+    //TODO refactor this, duplicate code for get jwt token
+    jwtTokenA = await getJwtToken(
+      httpServer,
+      createdUserStubA.email,
+      createdUserStubA.password,
+    );
+    jwtTokenB = await getJwtToken(
+      httpServer,
+      createdUserStubA.email,
+      createdUserStubA.password,
+    );
+    userInfoA = await getUserInfo(httpServer, jwtTokenA);
+    userInfoB = await getUserInfo(httpServer, jwtTokenB);
+
+    productFromUserA = await productService.create({
+      ...createProductStub,
+      user: { id: userInfoA.id },
+    });
+  });
+
+  it('should have jwt tokens', () => {
+    expect(jwtTokenA).toBeDefined();
+    expect(jwtTokenB).toBeDefined();
+  });
+
+  describe('/bid [POST] (integration)', () => {
+    beforeEach(async () => {
+      await cleanBidTable(bidRepository);
+    });
+
+    it('it should create a bid for product', async () => {
+      await request(httpServer)
+        .post(`/bid/${productFromUserA.id}`)
+        .set('Accept', 'application/json')
+        .set({
+          Authorization: `Bearer ${jwtTokenB}`,
+        })
+        .send(<CreateBidDto>{ value: 1.99 })
+        .expect(HttpStatus.CREATED);
+    });
+
+    it('it should throw error when try use same value for a bid', async () => {
+      await request(httpServer)
+        .post(`/bid/${productFromUserA.id}`)
+        .set('Accept', 'application/json')
+        .set({
+          Authorization: `Bearer ${jwtTokenB}`,
+        })
+        .send(<CreateBidDto>{ value: 1.99 })
+        .expect(HttpStatus.CREATED);
+
+      await request(httpServer)
+        .post(`/bid/${productFromUserA.id}`)
+        .set('Accept', 'application/json')
+        .set({
+          Authorization: `Bearer ${jwtTokenB}`,
+        })
+        .send(<CreateBidDto>{ value: 1.99 })
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+  });
 });
 
 export async function cleanBidTable(bidRepository) {
